c69afbd33458d3a4b14524a923e093171eaecef6
ShineTiger <555ysng@gmail.com>
2022-06-28 20:07:13 +0900

    sample code : 클래스 상속

--- JS/Core-javascript/Class.js
+++ JS/Core-javascript/Class.js
@@ -1,4 +1,16 @@
-class Person {
+//클래스 상속
+class Human {
+    constructor() {
+        this.from = 'korea'; // ① constructor() 생성자 함수를 추가
+    }
+
+    printFrom() {
+        console.log(this.from); 
+    }
+}
+
+class Person extends Human{ // ② extends 키워드를 이용해서 확장한다. 
+    super() // ③ 상위 클래스의 생성자 함수를 실행하는 super() 키워드를 추가 
     constructor() {
       this.name = 'me'; //property
     }
@@ -11,3 +23,5 @@ class Person {
   const person = new Person(); //new Person() 메소드를 사용해서 상수 person에 저장한다.
   person.printMyName()
   //me가 출력된다. 
+  person.printFrom()
+  // ④ Person 클래스에서도 Human 이 가지고있는 printFrom()을 호출할 수 있다. 
